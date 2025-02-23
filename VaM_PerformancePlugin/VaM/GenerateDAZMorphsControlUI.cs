7ee9e68d4d4a9bf34f5ceefcff3808d0c74d0a54
Playable2030 <Playable2030@proton.me>
2025-02-23 14:13:18 -0500

    Add WIP

--- VaM_PerformancePlugin/VaM/GenerateDAZMorphsControlUI.cs
+++ VaM_PerformancePlugin/VaM/GenerateDAZMorphsControlUI.cs
@@ -7,14 +7,25 @@ namespace VaM_PerformancePlugin.VaM;
 public class GenerateDAZMorphsControlUIPatch
 {
     // constructor patch to allow changing of default values via Plugin options
-    [HarmonyPatch(typeof(GenerateDAZMorphsControlUI), MethodType.Constructor)]
+    // [HarmonyPatch(typeof(GenerateDAZMorphsControlUI), MethodType.Constructor)]
+    // [HarmonyPostfix]
+    // public static void CTOR(ref GenerateDAZMorphsControlUI __instance)
+    // {
+    //     var Options = VaMPerformancePlugin.Options;
+    //     __instance.onlyShowActive = Options.Character_onlyShowActive.Value;
+    //     __instance.onlyShowFavorites = Options.Character_onlyShowFavorites.Value;
+    //     __instance.onlyShowLatest = Options.Character_onlyShowLatest.Value;
+    //     VaMPerformancePlugin.PluginLogger.LogDebug("Successfully finished patched CTOR for GenerateDAZMorphsControlUI");
+    // }
+    
+    [HarmonyPatch(typeof(GenerateDAZMorphsControlUI), "Awake")]
     [HarmonyPostfix]
-    public static void CTOR(ref GenerateDAZMorphsControlUI __instance)
+    public static void Awake(ref GenerateDAZMorphsControlUI __instance)
     {
         var Options = VaMPerformancePlugin.Options;
         __instance.onlyShowActive = Options.Character_onlyShowActive.Value;
         __instance.onlyShowFavorites = Options.Character_onlyShowFavorites.Value;
         __instance.onlyShowLatest = Options.Character_onlyShowLatest.Value;
-        VaMPerformancePlugin.PluginLogger.LogDebug("Successfully finished patched CTOR for GenerateDAZMorphsControlUI");
+        VaMPerformancePlugin.PluginLogger.LogDebug("Successfully finished patched Awake() for GenerateDAZMorphsControlUI");
     }
 }
