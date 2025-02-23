12ae47ca0fb2d4de17e2481cb9173e4bddc5a5ae
Playable2030 <Playable2030@proton.me>
2025-02-23 14:13:59 -0500

    Disable patch that may be causing bug - Security pop-up for new plugin permissions does not render correctly

--- VaM_PerformancePlugin/VaM/SuperController.cs
+++ VaM_PerformancePlugin/VaM/SuperController.cs
@@ -7,11 +7,11 @@ namespace VaM_PerformancePlugin.VaM;
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public class SuperControllerPatch
 {
-    [HarmonyPatch(typeof(SuperController), "CreateDynamicUIElement")]
-    [HarmonyPrefix]
-    public static bool CreateDynamicUIElement(ref RectTransform __result, ref RectTransform parent, ref RectTransform prefab)
-    {
-        __result = Object.Instantiate(prefab, parent, false);
-        return false;
-    }
+    // [HarmonyPatch(typeof(SuperController), "CreateDynamicUIElement")]
+    // [HarmonyPrefix]
+    // public static bool CreateDynamicUIElement(ref RectTransform __result, ref RectTransform parent, ref RectTransform prefab)
+    // {
+    //     __result = Object.Instantiate(prefab, parent, false);
+    //     return false;
+    // }
 }
